786286056c059d485bf95aef0af0fbd823b60b9a
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-08 00:02:52 +0300

    local paramethers and test

--- Local/local.cpp
+++ Local/local.cpp
@@ -4,12 +4,24 @@ int add(int a, int b) {
     return a +b;
 }
 
+void doMath(int a) {
+    int b = 5;
+    std::cout << "doMath: a = " <<a << "and b= " << b << std::endl;
+    a =4;
+    std::cout << "doMath: a = " <<a << "and b= " << b << std::endl;
+
+}
+
 int main() {
     int x = 7;
     int y =8;
-    int a =x;
-    int b=y;
+    int a = 6;
+    int b= 7;
     std::cout << add(a,b) << std::endl;
+
+    std::cout << "main: a = " <<a << "and b= " << b << std::endl;
+    doMath(a);
+    std::cout << "main: a = " <<a << "and b= " << b << std::endl;
 
     return 0;
 }
