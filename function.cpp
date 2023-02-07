72df4d3a4998b72ccdbbf7517b843b170fa0832d
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-07 22:58:53 +0300

    parametr

--- function.cpp
+++ function.cpp
@@ -4,6 +4,18 @@ int return5() {
     return 5;
 }
 
+void doPrint() {
+    std::cout << "In doPoint()" << std::endl;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int multiply(int c, int d) {
+    return c*d;
+}
+
 int getNumbers() {
     return 5;
     return 7;
@@ -15,6 +27,13 @@ int main() {
 
     std::cout << getNumbers() << std::endl;
     std::cout << getNumbers() << std::endl;
+
+    int a =15;
+    int b =30;
+
+    std::cout << "addition: " << add(a,b) << std::endl;
+    std::cout << "multiplication: " << multiply(2, 7) << std::endl;
+
 
 
     return5();
