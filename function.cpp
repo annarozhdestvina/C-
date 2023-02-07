1b4ec2ca77a938afbdb124f7a99690d2c93439cd
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-07 19:51:08 +0300

    function

---
+++ function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int return5() {
+    return 5;
+}
+
+int main() {
+    std::cout << "input"<<std::endl;
+    std::cout << return5() <<std::endl;
+
+    return5();
+    
+
+
+    return 0;
+}
