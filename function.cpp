0ddfe41aa36f890496c23e2feb381c22bc6b94f6
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-07 19:59:02 +0300

    done_lesson12

--- function.cpp
+++ function.cpp
@@ -4,9 +4,18 @@ int return5() {
     return 5;
 }
 
+int getNumbers() {
+    return 5;
+    return 7;
+}
+
 int main() {
     std::cout << "input"<<std::endl;
     std::cout << return5() <<std::endl;
+
+    std::cout << getNumbers() << std::endl;
+    std::cout << getNumbers() << std::endl;
+
 
     return5();
     
