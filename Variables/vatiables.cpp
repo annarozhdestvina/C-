7faaa1cecf4012afc80943e470266980c6f038fc
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-08 12:32:18 +0300

    names of variables

---
+++ Variables/vatiables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void MyFunctionName() {
+    
+}
+
+int main() {
+    int my_variableName;
+    void MyFuctionName();
+
+    int result;
+    int countFruits;
+    int kilograms_of_pipe;
+
+    return 0;
+}
