448b4f11eb2cac1a988c989e2eb361d3a6d6fd50
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-08 12:39:37 +0300

    trinary operator

--- Variables/vatiables.cpp
+++ Variables/vatiables.cpp
@@ -8,6 +8,12 @@ int main() {
     int my_variableName;
     void MyFuctionName();
 
+    int x =6;
+    int y =7;
+
+    int larger = (x>y) ? x : y;
+    std::cout << larger << std::endl;
+
     int result;
     int countFruits;
     int kilograms_of_pipe;
