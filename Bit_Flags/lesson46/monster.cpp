d383f7a0d57742ce5b56f3990be9cde4a4a96bc0
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-12 14:21:30 +0300

    some define of functions

---
+++ Bit_Flags/lesson46/monster.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void someFunction(unsigned int options);
+
+int main() {
+{
+    const unsigned char resistsPoison = 0x01; //шестнадцатеричный литерал для 0000 0001
+    const unsigned char resistsLightning = 0x02; //шестнадцатеричный литерал для 0000 0001
+    const unsigned char resistsFire = 0x04; //шестнадцатеричный литерал для 0000 0001
+    const unsigned char resistsCold = 0x08;
+    const unsigned char resistsTheft = 0x10;
+    const unsigned char resistsAcid = 0x20;
+    const unsigned char resistsTheft = 0x40;
+    const unsigned char resistsBlindness = 0x80;
+
+   // someFunction(option10 | option32); 
+
+}
+}
