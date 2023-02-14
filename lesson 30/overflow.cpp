5ee23c36e674801f12de817f17a2eb2edb9edc73
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-14 19:49:40 +0300

    overflow almost

---
+++ lesson 30/overflow.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    unsigned short x = 65535;
+    std::cout << "x was = " << x <<'\n';
+    x +=1;
+    std::cout << "x now = " << x << '\n';   
+}
---
+++ lesson 30/sizeof.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main () {
+    std::cout << "bool:\t\t" << sizeof(bool) << " bytes" << std::endl;
+    std::cout << "char:\t\t" << sizeof(char) << " bytes" << std::endl;
+    std::cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << std::endl;
+    std::cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << std::endl;  
+    std::cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << std::endl;    
+    std::cout << "short:\t\t" << sizeof(short) << " bytes" << std::endl;
+    std::cout << "int:\t\t" << sizeof(int) << " bytes" << std::endl;
+    std::cout << "long:\t\t" << sizeof(long) << " bytes" << std::endl;
+    std::cout << "long long:\t" << sizeof(long long) << " bytes" << std::endl;
+    std::cout << "float:\t\t" << sizeof(float) << " bytes" << std::endl;
+    std::cout << "double:\t\t" << sizeof(double) << " bytes" << std::endl;
+    std::cout << "long double:\t" << sizeof(long double) << " bytes" << std::endl;
+
+    int x=9;
+    std::cout << "x is " << sizeof(x) << " bites" << '\n';
+
+    signed char c;
+    signed long l;
+    std::cout << "signed long \t" << sizeof(l) << '\n';
+    return 0;
+}
