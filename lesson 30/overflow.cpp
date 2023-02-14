3c339fdb3ba3fb18255f84693a4daec0d33a385e
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-14 21:15:34 +0300

    new examples

--- lesson 30/overflow.cpp
+++ lesson 30/overflow.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstdint>
+//#include <cstdint>
 
 int main() {
     unsigned short x = 65535;
@@ -16,4 +16,15 @@ int main() {
 
     std::int8_t myint = 65;
     std::cout << myint << '\n';
+
+    float f = 9.847353426;
+    std::cout << f << '\n';
+    f = 0.00054322665665f;
+    std::cout << f << std::endl;
+
+    float b = 3.45e1;
+    std::cout << "b = " << b << '\n';
+
+    double g = 8e-10;
+    std::cout << "g= " << g << '\n';
 }
