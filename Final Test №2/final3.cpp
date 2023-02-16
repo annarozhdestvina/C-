e6fcbefefa218cb4bcca223accdc19ec0c9f5970
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-16 23:39:06 +0300

    task 3 is done

--- Final Test №2/final3.cpp
+++ Final Test №2/final3.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+void print_Result (double a, double b, char symbol) {
+    if (symbol == '+') 
+        std::cout <<a << symbol << b << " = "<< a+b << '\n';
+    if (symbol == '-') 
+        std::cout <<a << symbol << b << " = "<< a-b << '\n';
+    if (symbol == '*') 
+        std::cout <<a << symbol << b << " = "<< a*b << '\n';
+    if (symbol == '/') 
+        std::cout <<a << symbol << b << " = "<< a/b << '\n';
+
+}
+
 int main() {
     double a, b;
     std::cout << "enter two double " << '\n';
@@ -11,13 +23,7 @@ int main() {
     std::cout << "Enter one of the following: +, -, *, or /: *" << '\n';
     std::cin >> symbol;
 
-    if (symbol == '+') 
-        std::cout <<a << symbol << b << " = "<< a+b << '\n';
-    if (symbol == '-') 
-        std::cout <<a << symbol << b << " = "<< a-b << '\n';
-    if (symbol == '*') 
-        std::cout <<a << symbol << b << " = "<< a*b << '\n';
-    if (symbol == '/') 
-        std::cout <<a << symbol << b << " = "<< a/b << '\n';
-
+    print_Result(a, b, symbol);
+    
+    return 0;
 }
