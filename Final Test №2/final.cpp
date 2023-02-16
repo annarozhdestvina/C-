f557a60cb8409e15505b3827a9d0cc63e25d9c81
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-02-16 23:28:31 +0300

    second part of the test

---
+++ Final Test №2/final.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int8_t user_age; //cодержит только до 127 но самый старый чел =122
+
+    bool like_color;
+
+    const double pi = 3.1415;
+
+    const int16_t number_of_pages = 55;
+
+    double price_of_share; // float!!!!
+
+    const long long int winks = 9939848884; /// int32_t!!!!!
+
+    char option;
+
+    return 0;
+}
