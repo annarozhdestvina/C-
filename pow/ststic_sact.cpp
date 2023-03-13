4081918cee6849c7ef3c107050307feb7ed94565
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-03-13 13:45:46 +0300

    first try

---
+++ pow/ststic_sact.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+bool isEven(int o) {
+    if (o % 2) {
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    int x = 7; 
+    int y = 4;
+
+    std::cout << "int / int = " << x / y << "\n";
+    std::cout << "double / int = " << static_cast<double>(x) / y << "\n"; 
+    std::cout << "int / double = " << x / static_cast<double>(y) << "\n";
+    std::cout << "div with reminder 7/4 = " << x % y << "\n";
+
+    int count = 1;
+    while (count <= 100) {
+        std::cout << count << " ";
+        if(count % 30 == 0) {
+            std::cout << "\n";
+        }
+        count = count + 1;
+    }
+    std::cout << "\n";
+
+    double base, exp;
+    std::cout << "input base\n";
+    std::cin >> base;
+    std::cout << "input exp\n";
+    std::cin >> exp;
+
+    std::cout << "pow = " << pow(base, exp) << "\n";
+
+    int o;
+    std::cout << "input number\n";
+    std::cin >> o;
+    isEven(o);
+    bool a =isEven(o);
+    std::cout << a;
+
+    return 0;
+}
