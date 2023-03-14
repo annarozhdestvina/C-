562ca0c82327b3dca262145fb1a59c17110898ce
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-03-14 20:08:52 +0300

    first try of assignment

--- lessons/massive/cycles.cpp
+++ lessons/massive/cycles.cpp
@@ -31,7 +31,28 @@ int main() {
     }
     double averageScore = static_cast<double>(totalScore) / masStudents;
 
-    std::cout << "aver = " << averageScore;
+    std::cout << "aver = " << averageScore << '\n';
+
+    int array[] = {7, 5, 6, 4, 9, 8, 2, 1, 3};
+    int length_array = sizeof(array) / sizeof(array[0]);
+    for (int i = 0; i < length_array; i++) {
+        std::cout << array[i] << ' ';
+    }
+
+    int array_user[9];
+    std::cout << "input nember 1 to 9\n";
+    for (int i = 0; i < 9; i++) {
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(32767, '\n');
+        } else {
+            std::cin >> array_user[i];
+        }
+    }
+
+    for (int i = 0; i < 9; i++) {
+        std::cout << array_user[i] << ' ';
+    }
 
     return 0;
 }
