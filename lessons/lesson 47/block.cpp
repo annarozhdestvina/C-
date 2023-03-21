a3a48d78576466880080478d721fb4b5c7cf0245
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-03-21 15:12:00 +0300

    add block and condition if

--- lessons/lesson 47/block.cpp
+++ lessons/lesson 47/block.cpp
@@ -9,6 +9,16 @@ int main() {
     int b = 6;
     add(a, b);
     std::cout << "a+b = " << add(a, b) << '\n';
-    
+
+    std::cout <<"enter a value\n";
+    int value;
+    std::cin >> value;
+
+    if(value >= 0) {
+        std::cout << "value is positive " << value << '\n';
+    } else {
+        std::cout << "value is negative " << value << '\n';
+    }
+
     return 0;
 }
