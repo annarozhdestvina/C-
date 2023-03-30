59d94b1365262ef5ae79fe37c22786270bb31700
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-03-30 19:09:03 +0300

    with uniform-inicialization

--- lessons/lesson 61/struct.cpp
+++ lessons/lesson 61/struct.cpp
@@ -7,6 +7,12 @@ struct Employee
     double salary;
 };
 
+struct Triangle
+{
+    double length = 2.0;
+    double width = 3.0;
+};
+
 int main() {
     Employee john;
     john.id = 5555;
@@ -14,6 +20,8 @@ int main() {
     std::cout << john.id << '\n';
 
     Employee tom = {44, 24, 15000.0};
+
+    Triangle z{3.0, 8.0};
 
     return 0;
 }
