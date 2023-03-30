f5a4281c3a101d91a1e34a85d03b224ac6afa44d
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-03-30 19:12:05 +0300

    added function with output struct

--- lessons/lesson 61/struct.cpp
+++ lessons/lesson 61/struct.cpp
@@ -13,6 +13,12 @@ struct Triangle
     double width = 3.0;
 };
 
+void printInf(Employee employee) {
+    std::cout << "ID: " << employee.id << '\n';
+    std::cout << "age: " << employee.age << '\n';
+    std::cout << "salary: " << employee.salary << '\n';
+}
+
 int main() {
     Employee john;
     john.id = 5555;
@@ -20,6 +26,7 @@ int main() {
     std::cout << john.id << '\n';
 
     Employee tom = {44, 24, 15000.0};
+    printInf(tom);
 
     Triangle z{3.0, 8.0};
 
