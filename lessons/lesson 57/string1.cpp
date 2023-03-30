ac57d1ea0d30d6b63cac408955ca3aba739bda1e
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-03-30 13:29:51 +0300

    now it works correctly

--- lessons/lesson 57/string1.cpp
+++ lessons/lesson 57/string1.cpp
@@ -2,9 +2,11 @@
 #include <string>
 
 int main() {
-    std::cout << "pick 1 or 2\n";
+    std::cout << "pick 1 or 2: ";
     int choice;
     std::cin >> choice;
+
+    std::cin.ignore(32767, '\n'); // удаляем символ новой строки из входного потока данных
 
     std::cout << "Now enter your name: ";
     std::string myName;
