f5ffb5a8a20187c9408e1dd4f73419b42006e173
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-03-30 19:24:39 +0300

    test number 1 is done

--- lessons/lesson 61/struct.cpp
+++ lessons/lesson 61/struct.cpp
@@ -19,7 +19,34 @@ void printInf(Employee employee) {
     std::cout << "salary: " << employee.salary << '\n';
 }
 
+struct Advertising
+{
+    int shown;
+    double percentage;
+    double profit;
+};
+
+void calculate_profit(Advertising user) {
+    std::cout << "shown: " << user.shown << '\n';
+    std::cout << "per: " << user.percentage << '\n';
+    std::cout << "profit: " << user.profit << '\n';
+
+    double all = user.percentage * user.profit * user.shown;
+    std::cout << "all is: " << all << '\n';
+}
+
 int main() {
+    Advertising user1;
+    std::cout << "enter announcments\n";
+    std::cin >> user1.shown;
+    std::cout << "enter percentage\n";
+    std::cin >> user1.percentage;
+    std::cout << "enter profit\n";
+    std::cin >> user1.profit;
+
+    calculate_profit(user1);
+
+
     Employee john;
     john.id = 5555;
 
