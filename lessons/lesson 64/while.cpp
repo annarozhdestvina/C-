770692f3441e059fea82634b574766e97ad1a284
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-03 17:48:10 +0300

    tast 3 made with for. to do with while

--- lessons/lesson 64/while.cpp
+++ lessons/lesson 64/while.cpp
@@ -25,6 +25,54 @@ int main() {
         std::cout << symbol_begin << '\n';
         symbol_begin++;
     }
+/*
+    int outer1 = 5;
+    int inner1 =5;
+    while(outer1 >= 1) {
+        inner1 = 5;
+        while (inner1 <= outer1) 
+            std::cout << inner1-- << ' ';
+        outer1--;
+        std::cout << '\n';
+    }*/
+    
+
+    std::cout <<"----------------------\n";
+    int c = 5;
+    while(c > 0) {
+        std::cout << c << ' ';
+        c--;
+    }
+    std::cout <<"----------------------\n";
+
+    int a = 0;
+    int d = 1;
+   
+        while(d < 6) {
+            //std::cout << "a before " << a << '\n';
+            a=0;
+            while(a < d) {
+                std::cout << ++a << ' ';
+                }
+            std::cout << '\n';
+            //std::cout << "a after " << a << '\n';
+            d++;
+        }
+
+        a = 8%2;
+        std::cout <<a << '\n';
+        int q = 6;
+        for(int i =0; i < 6; i++) {
+            for (int j=0; j < 6; j++) {
+                if (q > 0) {
+                    std::cout << q << ' ';
+                    q--;
+                }
+            }
+            q = 5 - i;
+            std::cout << '\n';
+        }
+
 
     return 0;
 }
