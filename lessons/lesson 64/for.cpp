01031601c6d4cb78cfffab903f383139ad78db12
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-03 18:28:53 +0300

    test 1 is done

--- lessons/lesson 64/for.cpp
+++ lessons/lesson 64/for.cpp
@@ -5,4 +5,10 @@ int main() {
     for (aa = 0, bb = 9; aa < 10; aa++, bb--) {
         std::cout <<aa<<' ' << bb <<'\n';
     }
+
+    for (int a = 0; a < 21; a = a+2) {
+        std::cout << a << ' ';
+    }
+    std::cout << '\n';
+    return 0;
 }
