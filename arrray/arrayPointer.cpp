a263776eb3a58d390d6c5cff8363f307045fe208
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-08 12:18:12 +0300

    count vowels in any string

--- arrray/arrayPointer.cpp
+++ arrray/arrayPointer.cpp
@@ -9,5 +9,37 @@ int main() {
         std::cout << *(array+i)<< ' ';
     }
 
+    const int length = 9;
+    char name[length] = "Jonathan";
+    int numVowels(0);
+
+    std::cout << "enter a string: \n";
+    std::string myName;
+    std::getline(std::cin, myName);
+    int myLength = myName.length();
+    char Name[myLength];
+    for (int i =0; i < myLength; i++) {
+        Name[i] = myName[i];
+    }
+
+    std::cout << Name << '\n';
+
+    for(char* ptr = Name; ptr < Name + length; ptr++) {
+        switch(*ptr) {
+            case 'A':
+            case 'a':
+            case 'E':
+            case 'e':
+            case 'I':
+            case 'i':
+            case 'O':
+            case 'o':
+            case 'U':
+            case 'u':
+                ++numVowels;
+        }
+    }
+    std::cout << Name << " has " << numVowels << " vowels\n";
+
     return 0; 
 }
