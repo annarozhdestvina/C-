d2643c272b84d04886357a0f749b5eec8d2a86da
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-09 21:41:54 +0300

    file with leaks, to check it do leaks -atExit -- ./main.out | grep LEAK:

--- arrray/pointer.cpp
+++ arrray/pointer.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
+void doSomething() {
+    int *ptr = new int;
+}
+
 int main() {
     //new int;
+    doSomething();
     int* ptr = new int;
     *ptr = 8;
     std::cout << *ptr;
 
     int *ptr1 = new int (7);
-    int *ptr2 = new int {8};
+    int *ptr2 = new int{8};
 
     delete ptr;
     ptr = 0;
@@ -16,6 +21,17 @@ int main() {
     if(!value) {
         std::cout << "couldn't allocate memory\n";
     }
+
+    if(!ptr) 
+        ptr = new int;
+
+    if(ptr)
+        delete ptr;
+
+    int value1 = 7;
+    int *ptr3 = new int;
+    delete ptr3;
+    ptr3 = &value1;
 
     return 0;
 }
