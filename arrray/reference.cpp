5610845cc4b53033a46171ed158ea574237ff6e5
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-10 19:22:59 +0300

    struct with references

--- arrray/reference.cpp
+++ arrray/reference.cpp
@@ -8,7 +8,30 @@ void change_by_Pointer (int* a) {
     *a = 11;
 }
 
+struct Something {
+    int value1;
+    float value2;
+};
+
+struct Other {
+    Something my_something;
+    int otherValue;
+};
+
 int main() {
+    Other other;
+// работаем с полем value1 структуры Something 
+//     переменной other структуры Other
+
+//доступ к этому полю осуществлялся бы через other.something.value1
+    int& ref = other.my_something.value1;
+    other.my_something.value1 = 77;
+    std::cout << other.my_something.value1 << '\n';
+    //идентично этому 
+    ref = 88;
+    std::cout << ref << '\n';
+
+
     int x = 3;
     std::cout << "before change x: " << x <<'\n';
 
