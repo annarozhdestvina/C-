cb847aa6a5e63062566a6ccda8e77b2f2163d8f2
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-11 16:53:02 +0300

    clear working struct with finding max first and second with test number

--- arrray/get_struct_clear.cpp
+++ arrray/get_struct_clear.cpp
@@ -11,12 +11,6 @@ struct TestCase {
     int input_length;
 };
 
-enum Failuers {
-    OK = 0,
-    Unabled_array = 1,
-    Calc_array = 2,
-} errors;
-
 int allocate_array(TestCase *myTestCase) {
     int *array = new int[7] { 9, 7, 5, 3, 1, 8, 9 };
     int length = 7;
@@ -59,7 +53,6 @@ void test_case(TestCase *myTestCase, int* test_number, int* succesful_test, int 
             std::cout << "actual is\t" << myTestCase->actual << '\n';
             std::cout << "expected is\t" << myTestCase->expected << '\n';
         }
-    print_struct(myTestCase);
 }
 void get_product_test(int is_verbose) {
     int test_number = 1;
@@ -67,7 +60,6 @@ void get_product_test(int is_verbose) {
     {
         TestCase myTestCase;
         allocate_array(&myTestCase);
-        print_struct(&myTestCase);
         const int actual = 81;
         const int expected = get_product(&myTestCase);
 
@@ -76,7 +68,40 @@ void get_product_test(int is_verbose) {
 
         test_case(&myTestCase, &test_number, &succesful_test, is_verbose);
     }
-    //test_number++;
+    test_number++;
+
+    {
+        TestCase myTestCase;
+        int *array = new int[7] { 0, 7, 5, 3, 1, 8, 0 };
+        int length = 7;
+        myTestCase.input = array;
+        myTestCase.input_length = length;
+
+        const int actual = 56;
+        const int expected = get_product(&myTestCase);
+
+        myTestCase.actual = actual;
+        myTestCase.expected = expected;
+
+        test_case(&myTestCase, &test_number, &succesful_test, is_verbose);
+    }
+    test_number++;
+
+    {
+        TestCase myTestCase;
+        int *array = new int[5] { 0, 7, 5, 3, 1 };
+        int length = 5;
+        myTestCase.input = array;
+        myTestCase.input_length = length;
+
+        const int actual = 35;
+        const int expected = get_product(&myTestCase);
+
+        myTestCase.actual = actual;
+        myTestCase.expected = expected;
+
+        test_case(&myTestCase, &test_number, &succesful_test, is_verbose);
+    }
     
    
     std::cout << "\nSUMMORY\n";
