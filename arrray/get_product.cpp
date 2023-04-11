2feeb12d41ca0c8c43c40c5d24b6792a1e97215e
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-11 12:32:09 +0300

    get_product_test

--- arrray/get_product.cpp
+++ arrray/get_product.cpp
@@ -11,14 +11,48 @@ void print_array(const int* array, int length) {
     std::cout << '\n';
 }
 
+void get_product_test() {
+    int test_number = 1;
+    {
+        std::cout << "test #" << test_number << "\t\t";
+        const int array[] = {1, 2, 3, 4, 5};
+        const int length = sizeof(array) / sizeof(array[0]);
+        const int expected = 20;
+        const int actual = get_product(array, length);
+
+        if(actual == expected) {
+            std::cout << "ok\n";
+        } else {
+            std::cout << "FAILED\n";
+            std::cout << "actual is\t" << actual << '\n';
+            std::cout << "expected is\t" << expected << '\n';
+        }
+
+
+        print_array(array, length); 
+    } test_number++;
+
+    {
+        std::cout << "test #" << test_number << "\t\t";
+        const int array[] = {0, 2, 3, 0, 5};
+        const int length = sizeof(array) / sizeof(array[0]);
+        const int expected = 15;
+        const int actual = get_product(array, length);
+
+        if(actual == expected) {
+            std::cout << "ok\n";
+        } else {
+            std::cout << "FAILED\n";
+            std::cout << "actual is\t" << actual << '\n';
+            std::cout << "expected is\t" << expected << '\n';
+        }
+        print_array(array, length); 
+    } test_number++;
+}
+
 int main() {
-    const int array[] = {1, 2, 3, 4, 5};
-    const int length = sizeof(array) / sizeof(array[0]);
 
-    std::cout << length << '\n';
-    print_array(array, length); 
-
-
+    get_product_test();
 
     return 0;
 }
