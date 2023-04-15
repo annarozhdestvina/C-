8e8329381d6ded801ad25112f5848fef161fdd55
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-15 14:06:21 +0300

    reference function with radians translation

--- lessons/lesson 98/reference.cpp
+++ lessons/lesson 98/reference.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <math.h>
+
+void getSinCos(double degrees, double& sinOut, double& cosOut) {
+    const double pi = 3/1415926535;
+    double radians = degrees * pi / 180;
+    sinOut = sin(radians);
+    cosOut = cos(radians);
+}
 
 void fucc(int &x) {
     x = x+2;
@@ -9,6 +17,13 @@ int main() {
     std::cout << x << '\n';
     fucc(x);
     std::cout << x << '\n';
+
+    double sin(0.0);
+    double cos(0.0);
+
+    getSinCos(30.0, sin, cos);
+    std::cout << "The sin is " << sin << '\n';
+    std::cout << "The cos is " << cos << '\n';
 
 
     return 0;
