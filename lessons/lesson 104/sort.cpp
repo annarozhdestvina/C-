0bd78f2bc98271e7234b1747bb30657ae9061b87
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-17 14:22:16 +0300

    fun pointer sort descending and ascending

--- lessons/lesson 104/sort.cpp
+++ lessons/lesson 104/sort.cpp
@@ -1,14 +1,14 @@
 #include <algorithm>
 #include <iostream>
 
-void SelectionSort (int* array, int size) {
+void SelectionSort (int* array, int size, bool (*comparison)(int, int)) {
     for (int start = 0; start < size; start++) {
-        int smallest = start;
+        int best = start;
         for(int current = start + 1; current < size; current++) {
-            if(array[smallest] > array[current])
-                smallest = current;
+            if(comparison(array[best], array[current]))
+                best = current;
         }
-        std::swap(array[start], array[smallest]);
+        std::swap(array[start], array[best]);
     }
 }
 
@@ -18,13 +18,26 @@ void printArray(int* array, int size) {
     }
     std::cout << '\n';
 }
+
+bool ascending(int a, int b) {
+    return a > b;
+}
+
+bool descending(int a, int b) {
+    return a < b;
+}
  
 int main() {
     int array[7] = {3, 55, 6, 2, 9, 0, 1};
     int size = sizeof(array) / sizeof(array[0]);
+    //printArray(array, size);
+
+    // Сортируем массив в порядке убывания
+    SelectionSort(array, size, descending);
     printArray(array, size);
 
-    SelectionSort(array, size);
+    // Сортируем массив в порядке возрастания
+    SelectionSort(array, size, ascending);
     printArray(array, size);
 
 
