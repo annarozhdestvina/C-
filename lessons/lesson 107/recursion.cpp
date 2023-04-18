6e628ffeb491aca0a7a274cbcf3176621743baae
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-18 18:46:29 +0300

    first task about factorial

--- lessons/lesson 107/recursion.cpp
+++ lessons/lesson 107/recursion.cpp
@@ -17,7 +17,11 @@ int sumCount(int value) {
 
 int factorial(int n) {
     int res =1;
-    for(res; n>1; res *= (n--));
+    if(n < 0) {
+        std::cout << "input positive number!\n";
+        return 0;
+    } else 
+        for(res; n>1; res *= (n--));
     return res;
 }
 
@@ -27,10 +31,23 @@ int sumcout(int n) {
     return res;
 }
 
+int fibonacci(int number)
+{
+    if (number == 0)
+        return 0; // базовый случай (условие завершения)
+    if (number == 1)
+        return 1; // базовый случай (условие завершения)
+    return fibonacci(number-1) + fibonacci(number-2);
+}
+
 int main() {
     countOUT(4);
     std::cout << "sum " <<sumCount(5) << '\n';
-    std::cout << "fac " << factorial(4) << '\n';
+    std::cout << "fac " << factorial(5) << '\n';
     std::cout << "mysum " << sumcout(5) << '\n';
+
+    for (int count=0; count < 13; ++count)
+        std:: cout << fibonacci(count) << " ";
+ 
     
 }
