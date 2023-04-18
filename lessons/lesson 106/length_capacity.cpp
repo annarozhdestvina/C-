66ae9120a0040b0a9f38f1b17c01db08d4310d8d
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-18 16:29:03 +0300

    pop stack and how to add elements to stack

--- lessons/lesson 106/length_capacity.cpp
+++ lessons/lesson 106/length_capacity.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 #include <vector>
 
+void printStack(const std::vector<int> &stack) {
+    for (const auto &element : stack)
+        std::cout << element << ' ';
+    std::cout << "cap " << stack.capacity() << " length " << stack.size() << '\n';
+}
+
 int main() {
+    std::vector<int> stack;
+    printStack(stack);
+    stack.push_back(7); // add element 7
+    printStack(stack);
+
+    stack.push_back(4); // add element 7
+    printStack(stack);
+    stack.push_back(1); // add element 7
+    printStack(stack);
+
+    std::cout << "top: " << stack.back() << '\n';
+
+    stack.pop_back();
+    printStack(stack);
+    stack.pop_back();
+    printStack(stack);
+    stack.pop_back();
+    printStack(stack);
+
+
+
+
     //int* array = new int[12] {1, 5, 4, 3, 2, 5};
     std::vector<int> array {0, 3, 4, 2};
     array.resize(6); // устанавливаем длину, равную 6
