928d346feb20cb979b52b5a97308ba7ee39d0340
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-24 16:23:09 +0300

    class with private members task about isEqual

--- oop/example1/lesson 114/public.cpp
+++ oop/example1/lesson 114/public.cpp
@@ -37,6 +37,13 @@ public:
     void print() {
         std::cout <<"< " << m_a<<", "<<m_b<<", "<<m_c<< ">\n";
     }
+
+    int isEqual(const Numbers &b) {
+        if(m_a == b.m_a && m_b == b.m_b && m_c == b.m_c)
+            return true;
+        else 
+            return false;
+    }
 };
 
 int main() {
@@ -52,6 +59,26 @@ int main() {
     Numbers points;
     points.setValues(3.0, 4.66, 7.44);
     points.print();
+
+    Numbers point1;
+    point1.setValues(3.0, 4.0, 5.0);
+ 
+    Numbers point2;
+    point2.setValues(3.0, 4.0, 5.0);
+ 
+    if (point1.isEqual(point2))
+        std::cout << "point1 and point2 are equal\n";
+    else
+        std::cout << "point1 and point2 are not equal\n";
+ 
+    Numbers point3;
+    point3.setValues(7.0, 8.0, 9.0);
+ 
+    if (point1.isEqual(point3))
+        std::cout << "point1 and point3 are equal\n";
+    else
+        std::cout << "point1 and point3 are not equal\n";
+ 
 
 
 
