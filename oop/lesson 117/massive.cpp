0ab98127cd254b8ff78d191df98fa15e00d5c16d
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-26 17:37:00 +0300

    create massive and print it

---
+++ oop/lesson 117/massive.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Values {
+private:
+    const int m_array[7];
+
+public:
+    Values(): m_array {1, 2, 3, 4, 5, 6} {}
+
+    void print() {
+        for(int i = 0; i < 6; i++)
+            std::cout << m_array[i] << ' ';
+    }
+};
+
+int main() {
+    Values array;
+    array.print();
+}
