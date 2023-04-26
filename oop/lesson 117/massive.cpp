00f0762665b0784be849bb259e628b9968bc729b
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-04-26 18:25:09 +0300

    zero massive

--- oop/lesson 117/massive.cpp
+++ oop/lesson 117/massive.cpp
@@ -5,7 +5,9 @@ private:
     const int m_array[7];
 
 public:
-    Values(): m_array {1, 2, 3, 4, 5, 6} {}
+   // Values(): m_array {1, 2, 3, 4, 5, 6} {}
+
+    Values(): m_array {} {}
 
     void print() {
         for(int i = 0; i < 6; i++)
