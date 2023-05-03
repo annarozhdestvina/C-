e31191aba45826fe886331851f528ac08706f2de
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-05-03 14:39:22 +0300

    overloadint operator * to multiply fractions in class

--- oop/overloading/lesson 131/test.cpp
+++ oop/overloading/lesson 131/test.cpp
@@ -9,16 +9,26 @@ public:
     Fraction(int numerator = 0, int denumerator = 1) :
     m_numerator(numerator), m_denumerator(denumerator) {}
 
-    //friend Fraction operator/(int num, int den);
+    friend Fraction operator*(int value, const Fraction& f1);
+    friend Fraction operator*(const Fraction& f1, int value);
+    friend Fraction operator*(const Fraction& f1, const Fraction& f2);
 
     void print() {
         std::cout << m_numerator << '/' << m_denumerator <<'\n';
     } 
 };
 
-/*Fraction operator/ (int num, int den) {
-    std::cout << num << '/' << den<< '\n';
-}*/
+Fraction operator*(int value, const Fraction& f1) {
+    return Fraction(f1.m_numerator * value);
+}
+
+Fraction operator*(const Fraction& f1, int value) {
+    return Fraction(f1.m_numerator * value);
+}
+
+Fraction operator*(const Fraction& f1, const Fraction& f2) {
+    return Fraction(f1.m_numerator * f2.m_numerator, f1.m_denumerator * f2.m_denumerator);
+}
 
 int main() {
     Fraction f1(4, 77);
@@ -26,6 +36,18 @@ int main() {
 
     Fraction f2(1, 2);
     f2.print();
+
+    Fraction f3 = 5 * f1;
+    f3.print();
+
+    Fraction f4 = f2 * 3;
+    f4.print();
+
+    Fraction f5 = f1 * f2;
+    f5.print();
+
+    Fraction f6 = Fraction(1, 2) * Fraction(2, 3) * Fraction(3, 4);
+    f6.print();
 
     return 0;
 }
