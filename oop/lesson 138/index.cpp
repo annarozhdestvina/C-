bc32c658c72aa0aa3a95108b85624aecdbacaf08
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-05-16 14:31:36 +0300

    index array

--- oop/lesson 138/index.cpp
+++ oop/lesson 138/index.cpp
@@ -5,6 +5,8 @@ private:
     int m_array[10];
 
 public:
+    int& operator[] (const int index);
+
     void setItem(int index, int value) {
         m_array[index] = value;
     }
@@ -14,9 +16,17 @@ public:
     }
 };
 
+int& IntArray::operator[] (const int index) {
+    return m_array[index];
+}
+
 int main() 
 {
     IntArray array;
     array.setItem(4, 5);
-    std::cout << array.getItem(4);
+    std::cout << array.getItem(4) << '\n';
+
+    IntArray array2;
+    array2[4] = 6;
+    std::cout << array2[4] << std::endl;
 }
