3b1aa3a1d9106a0656d0ea92a5c7306d7277bd15
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-05-17 13:53:08 +0300

    overloading operator() to clean matrix with 0

--- oop/lesson 139/matrix.cpp
+++ oop/lesson 139/matrix.cpp
@@ -14,10 +14,39 @@ public:
     double& operator()(int row, int col);
     const double& operator()(int row, int col) const;
 
-    void print();
+    void Print();
+    void Print() const;
+    void operator()();
+
+    void Fill();
 };
 
-void Matrix::print() {
+void Matrix::operator()() {
+for (int row = 0; row < 5; row++) 
+        for(int col = 0; col < 5; col++) 
+            data[row][col] = 0.0;
+}
+
+
+void Matrix::Fill() {
+    int k = 1;
+    for (int row = 0; row < 5; row++) {
+        for(int col = 0; col < 5; col++) {
+            data[row][col] = ++k;
+        }
+    }
+}
+void Matrix::Print() const {
+    for (int row = 0; row < 5; row++) {
+        for(int col = 0; col < 5; col++) {
+            std::cout << data[row][col] << ' ';
+        }
+        std::cout <<'\n';
+    }
+    
+}
+
+void Matrix::Print() {
     for (int row = 0; row < 5; row++) {
         for(int col = 0; col < 5; col++) {
             std::cout << data[row][col] << ' ';
@@ -44,6 +73,12 @@ const double& Matrix::operator()(int row, int col) const {
 int main() {
     Matrix matrix;
     matrix(2, 3) = 3.6;
-    matrix.print();
+    matrix.Print();
+    std::cout << '\n';
+    matrix();
+    matrix.Print();
+
+    const Matrix m2;
+    // m2.Print();
     
 }
