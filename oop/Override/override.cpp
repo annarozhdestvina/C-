8a0fef044a2383d763683c128597280579269937
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-07-28 13:53:09 +0300

    ovrride

---
+++ oop/Override/override.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ 
+class A
+{
+public:
+	virtual const char* getName1(int x) { return "A"; }
+	virtual const char* getName1(short int x) { return "A"; }
+	virtual const char* getName2(int x) { return "A"; }
+	virtual const char* getName3(int x) { return "A"; }
+};
+ 
+class B : public A
+{
+public:
+	virtual const char* getName1(short int x) override { return "B"; }  // ошибка компиляции, метод не является переопределением
+	virtual const char* getName2(int x) const override { return "B"; }  // ошибка компиляции, метод не является переопределением
+	virtual const char* getName3(int x) override { return "B"; }  // всё хорошо, метод является переопределением A::getName3(int)
+ 
+};
+ 
+int main()
+{
+	return 0;
+}
