701b56018aebb1ae537dd52c0168f126c2cc29ac
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-08-01 18:55:49 +0300

    template for char and double

--- template/lesson 177/explicit.cpp
+++ template/lesson 177/explicit.cpp
@@ -23,6 +23,29 @@ void Repository<double>::print()
     std::cout << std::scientific << m_value << '\n';
 }
 
+template <>
+Repository<char*>::Repository(char* value)
+{
+    // Определяем длину value
+    int length=0;
+    while (value[length] != '\0')
+        ++length;
+    ++length; // +1, учитывая нуль-терминатор
+ 
+    // Выделяем память для хранения значения value
+    m_value = new char[length];
+ 
+    // Копируем фактическое значение value в m_value
+    for (int count=0; count < length; ++count)
+        m_value[count] = value[count];
+}
+
+template <>
+Repository<char*>::~Repository()
+{
+    delete[] m_value;
+}
+
 int main() {
     // Инициализируем объекты класса
     Repository<int> nValue(7);
@@ -31,4 +54,20 @@ int main() {
     // Выводим значения объектов класса
     nValue.print();
     dValue.print();
+
+    // Динамически выделяем временную строку
+    char *string = new char[40];
+ 
+    // Просим пользователя ввести свое имя
+    std::cout << "Enter your name: ";
+    std::cin >> string;
+ 
+    // Сохраняем то, что ввел пользователь
+    Repository<char*> repository(string);
+ 
+    // Удаляем временную строку
+    delete[] string;
+ 
+    // Пытаемся вывести то, что ввел пользователь
+    repository.print(); // получаем мусор
 }
