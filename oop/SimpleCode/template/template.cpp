aee5844df02d1a2944e103a22101ee0f648fc4fe
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-09-05 15:54:28 +0300

    two types of template class

--- oop/SimpleCode/template/template.cpp
+++ oop/SimpleCode/template/template.cpp
@@ -18,36 +18,51 @@ public:
     int z;
 };
 
-template<typename T>
+template<typename T, typename T2>
 class MyClass {
 private:
     T m_value;
+    T2 m_value2;
 
 public:
     void DataTypeSize() {
         std::cout << sizeof(m_value) << '\n';
         std::cout << typeid(m_value).name() << '\n';
+        std::cout << sizeof(m_value2) << '\n';
+        std::cout << typeid(m_value2).name() << '\n';
     }
 
     // MyClass(T value) : m_value(value) {}
-    MyClass(T value) {
+    MyClass(T value, T2 value2) {
         this->m_value = value;
+        this->m_value2 = value2;
+    }
+
+    T Foo() {
+        return m_value;
     }
 };
 
 
 int main() {
     int a = 5;
+    double b = 6.66;
 
-    MyClass<int> c(a);
-    MyClass n(a); // так как класс шаблонный мы не можем просто создать
+    MyClass<int, double> c(a, b);
+    MyClass n(a, b); // так как класс шаблонный мы не можем просто создать
     // обьект этого класаа без указания типа и передать туда переменную
     // а в 17 версии можно
+    n.DataTypeSize();
 
     // при создания шаблолнного класса указать тип 
 
-    MyClass g = (88.9);
+    MyClass g (88.9, 6.f);
     g.DataTypeSize();
+    g.Foo();
+
+    Point point;
+    MyClass clas(point, a);
+    clas.DataTypeSize();
 
     return 0;
 }
