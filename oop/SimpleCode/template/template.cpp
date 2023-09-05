aa03e49b6e4ba33c9ba408ccce72c41ea2d35bc8
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-09-05 14:42:50 +0300

    через шабдонный класс не можем просто так создать объект

--- oop/SimpleCode/template/template.cpp
+++ oop/SimpleCode/template/template.cpp
@@ -29,11 +29,22 @@ public:
         std::cout << typeid(m_value).name() << '\n';
     }
 
-    MyClass(T value) : m_value(value) {}
+    // MyClass(T value) : m_value(value) {}
+    MyClass(T value) {
+        this->m_value = value;
+    }
 };
 
 
 int main() {
+    int a = 5;
+
+    MyClass<int> c(a);
+    MyClass n(a); // так как класс шаблонный мы не можем просто создать
+    // обьект этого класаа без указания типа и передать туда переменную
+    // а в 17 версии можно
+
+    // при создания шаблолнного класса указать тип 
 
     MyClass g = (88.9);
     g.DataTypeSize();
