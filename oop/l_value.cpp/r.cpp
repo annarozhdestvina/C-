36b0e78c8006935816fdfd26944d4728172b0b27
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-10-05 18:18:43 +0300

    r-value l-value with test

--- oop/l_value.cpp/r.cpp
+++ oop/l_value.cpp/r.cpp
@@ -14,10 +14,24 @@ public:
     }
 };
 
+void fun(const int &lref) {
+    std::cout << "l-value ref to const\n";
+}
+
+void fun(int &&rref) {
+    std::cout << "r-value ref\n";
+}
+
 
 int main() {
     Fraction &&ref = Fraction(4, 7);// ссылка r-value на анонимный объект
     std::cout << ref <<'\n';
 
+    int x = 7;
+    fun(x); // аргумент l-value вызывает функцию с ссылкой l-value
+	fun(7); // аргумент r-value вызывает функцию с ссылкой r-value
+
+    int x = 8;
+    const int &&ref = 9;
     return 0;
 }
