b05c9dfb80e2023539f6cca950d4a64d7c608a66
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-10-05 22:43:06 +0300

    copy constructor

---
+++ oop/move_semantics/lesson 191/move.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+
+template<class T>
+class Auto_ptr3 {
+    T* m_ptr;
+public:
+    Auto_ptr3(T* ptr = nullptr) : m_ptr(ptr) {}
+
+    ~Auto_ptr3() {
+        delete m_ptr;
+    }
+
+    Auto_ptr3(const Auto_ptr3 &x) {
+        m_ptr = new T;
+        *m_ptr = *x.m_ptr;
+    }
+
+    Auto_ptr3& operator=(const Auto_ptr3 &x) {
+        if(&x == this)
+            return *this;
+        
+        delete m_ptr;
+
+        m_ptr = new T;
+        *m_ptr = *x.m_ptr;
+        return *this;
+    }
+
+    T& operator*() const {
+        return *m_ptr;
+    }
+
+    T* operator->() const {
+        return m_ptr;
+    }
+
+    bool isNull() const {
+        return m_ptr == nullptr;
+    }
+};
+
+
+class Item {
+public:
+	Item() { std::cout << "Item acquired\n"; }
+	~Item() { std::cout << "Item destroyed\n"; }
+};
+
+Auto_ptr3<Item> generatorItem() {
+    Auto_ptr3<Item> item(new Item);
+    return item;
+}
+
+int main() {
+    Auto_ptr3<Item> mainItem;
+    mainItem = generatorItem();
+
+    return 0;
+}
