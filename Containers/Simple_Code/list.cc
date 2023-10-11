78ad1c753f3de9b7ee33cb6f905272147163f487
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-10-11 16:49:11 +0300

    list push back

--- Containers/Simple_Code/list.cc
+++ Containers/Simple_Code/list.cc
@@ -1,20 +1,48 @@
 // list 
+#include <iostream>
 
+template<typename T>
 class List {
 public:
-    List() {}
-    ~List() {}
+    List();
+    ~List();
+    void push_back(T data);
 
 private:
-    template<typename T>
+    template<typename T2>
     class Node {
     public:
         Node *pNext;
         T data;
 
-        Node(T data = T(), Node *pNext = nullptr) {
+        Node(T2 data = T(), Node *pNext = nullptr) {
             this->data = data;
             this->pNext = pNext;
         }
     };
+    int Size;
+
+    Node<T> *head;
 };
+
+template<typename T>
+List<T>::List() {
+    Size = 0;
+    head = nullptr;
+}
+
+template<typename T>
+List<T>::~List() {}
+
+template<typename T>
+void List<T>::push_back(T data) {
+    if(head == nullptr)
+        head = new Node<T>(data);
+
+}
+
+int main() {
+    List<int> lst;
+    lst.push_back(5); 
+    return 0;
+}
