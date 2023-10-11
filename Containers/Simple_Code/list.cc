ca79846dde927377dbf4a0b35606245e6727e1f2
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-10-11 16:00:28 +0300

    list realization

---
+++ Containers/Simple_Code/list.cc
@@ -0,0 +1,20 @@
+// list 
+
+class List {
+public:
+    List() {}
+    ~List() {}
+
+private:
+    template<typename T>
+    class Node {
+    public:
+        Node *pNext;
+        T data;
+
+        Node(T data = T(), Node *pNext = nullptr) {
+            this->data = data;
+            this->pNext = pNext;
+        }
+    };
+};
